8b1f299582ab4d8f8a5d1ce7547eb7b97d010757
Thomas Tran <tkt2120@columbia.edu>
2021-04-12 16:06:31 -0400

    remove old code from params

--- config/params.py
+++ config/params.py
@@ -1,28 +1,11 @@
 
 # CONFIG FOR STARTER
 """
-dense_params = {'model'      : 'dense',
-              'model_params': {'m'          : 20,
-                               'n'          : 10,
-                               't'          : 10,
-                               'lr'         : 0.001}
-              }
-
-attention_params = {'model'      : 'attention',
-                   'model_params': {'n'          : 10,
-                                    'h'          : 6,
-                                    'lr'         : 0.005}
-                  }
-
 # todo: fix parallelization for rnns
 rnn_params = {'model'      : 'rnn',
               'model_params': {'n'          : 10,
                                'lr'         : 0.001}
             }
-
-rand_params = {'model'      : 'random',
-              'model_params': { }
-             }
 """
 
 def attention_params(n, h, lr):
